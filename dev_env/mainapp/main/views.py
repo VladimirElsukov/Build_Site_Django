b354d6e736a0a27e34e5c072f1f52d779d20f011
Vladimir Elsukov <groupdirect@yandex.ru>
2024-07-09 14:54:53 +0300

    Создали приложение goods

--- dev_env/mainapp/main/views.py
+++ dev_env/mainapp/main/views.py
@@ -3,13 +3,17 @@ from django.shortcuts import render
 
 def index(request):
     context: dict = {
-        'title': 'Home',
-        'content': 'Главная страница магазина- Home',
-        'list': ['наименование', 'цена'],
-        'dict': {'colour': 'красный'},
-        'is_authenticated': True
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели Home',
+        
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page')
+    context: dict = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Осветить преимущества магазина, его выделение на фоне конкурентов.',
+        
+    }
+    return render(request, 'main/about.html', context)

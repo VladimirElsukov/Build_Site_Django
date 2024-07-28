d5fa13116895d8b7c94b676595bb69df0a7030a2
Vladimir Elsukov <groupdirect@yandex.ru>
2024-07-28 17:47:07 +0300

    Создали конвертер Slug в название продукта

--- dev_env/mainapp/goods/views.py
+++ dev_env/mainapp/goods/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from goods.models import Products
 
- 
 
 def catalog(request):
     goods = Products.objects.all()
+
     context = {
         "title": "Home - Каталог",
         "goods": goods,
     }
+
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {'product': product}
+    return render(request, "goods/product.html", context=context)

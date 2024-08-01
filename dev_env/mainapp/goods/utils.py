8248cb9cf8687096916a455ed6a9e378e3dfb3a5
Vladimir Elsukov <groupdirect@yandex.ru>
2024-08-01 18:59:57 +0300

    Реализован алгоритм  поиска товаров по названию и описанию

--- dev_env/mainapp/goods/utils.py
+++ dev_env/mainapp/goods/utils.py
@@ -1,5 +1,15 @@
+from django.db.models import Q
+
 from goods.models import Products
 
 def g_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    keywords = [word for word in query.split() if len(word) > 2]
+    # алгоритм фильтрации по названию и описанию через Q токен
+    q_objects = Q()
+    for token in keywords:
+        q_objects |= Q(name__icontains=token)
+        q_objects |= Q(description__icontains=token)
+
+    return Products.objects.filter(q_objects)

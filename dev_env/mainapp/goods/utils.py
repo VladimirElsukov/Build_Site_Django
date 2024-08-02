8006fd9ef138dd55bf74c0b91d02c4b7eae9e98a
Vladimir Elsukov <groupdirect@yandex.ru>
2024-08-02 15:16:00 +0300

    Изменили базу данных на PostgreSQL

--- dev_env/mainapp/goods/utils.py
+++ dev_env/mainapp/goods/utils.py
@@ -3,10 +3,12 @@ from django.db.models import Q
 from goods.models import Products
 
 def g_search(query):
+    # Алгоритм поиска по id товара
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     keywords = [word for word in query.split() if len(word) > 2]
-    # алгоритм фильтрации по названию и описанию через Q токен
+
+    # алгоритм поиска товара по названию и описанию через Q токен
     q_objects = Q()
     for token in keywords:
         q_objects |= Q(name__icontains=token)

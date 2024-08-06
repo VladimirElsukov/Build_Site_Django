be594bbd49cc8102ffd76cb119f8c1b919af079f
Vladimir Elsukov <groupdirect@yandex.ru>
2024-08-06 16:53:50 +0300

    Полностью реализована регистрационная форма и контроллеры

--- dev_env/mainapp/users/views.py
+++ dev_env/mainapp/users/views.py
@@ -1,10 +1,11 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 from django.shortcuts import render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 # Create your views here.
 def login(request):
@@ -28,8 +29,20 @@ def login(request):
 
 
 def registration(request):
-    context = {"title": "Home - Регистрация"}
-    return render(request, "users/registration.html", context)
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+    context = {
+        "title": "Home - Регистрация",
+        "form": form
+    }
+    return render(request, 'users/registration.html', context)
 
 
 def profile(request):
@@ -38,4 +51,5 @@ def profile(request):
 
 
 def logout(request): 
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
